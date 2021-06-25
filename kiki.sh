d9386664ac64c88444fbbc85013a6bfee84b9318
taebk2838 <Yawry66383@outlook.com>
2021-06-25 23:03:41 +0530

    commit messadjs

--- kiki.sh
+++ kiki.sh
@@ -15,7 +15,7 @@ do
    sleep 2
    git commit -m "commit messadjs" 
    sleep 2
-   git push origin main --force
+   git push origin master --force
    sleep 3
    rm teemy.txt
    sleep 2
@@ -23,7 +23,7 @@ do
    sleep 2
    git commit -m "commit messadjs" 
    sleep 2
-   git push origin main --force   
+   git push origin master --force   
    now=$(date +"%T")
    echo "Current time : $now"
    echo -e "${RED}current time :$now ${ENDCOLOR}"
